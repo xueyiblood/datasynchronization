e7b8f6529e3e8ffa215f46ffe784da68750a2073
10097 <1009796817@qq.com>
2020-09-15 10:34:45 +0800

    demo提交

--- src/main/java/com/yc/datasynchronization/DatasynchronizationApplication.java
+++ src/main/java/com/yc/datasynchronization/DatasynchronizationApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,7 +13,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @SpringBootApplication(exclude = {MybatisAutoConfiguration.class, DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class,})
 @EnableScheduling
-public class DatasynchronizationApplication {
+public class DatasynchronizationApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatasynchronizationApplication.class, args);
